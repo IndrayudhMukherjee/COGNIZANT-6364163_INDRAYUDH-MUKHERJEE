646b023faad25c9c6540958ca12a04c1eee67efa
Indrayudh <145827263+IndrayudhMukherjee@users.noreply.github.com>
2025-07-26 15:19:59 +0530

    Add files via upload

---
+++ WEEK6/STUDENTAPP1/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Namaste! 🙏</h2>
+        <p>
+          Welcome to the About page of the Student Management Portal. Here, we
+          believe in "Padhega India, Tabhi Toh Badhega India" 🇮🇳.
+          <br />
+          This platform is built with ❤️ to support students across Bharat
+          in their academic journey.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ WEEK6/STUDENTAPP1/Contact.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Have any doubts? Chinta mat karo!</h2>
+        <p>
+          You're on the Contact page of the Student Management Portal. Whether
+          it's a syllabus confusion or attendance ka masla, we’re just a call
+          or chai break away ☕📞.
+          <br />
+          Drop us a message and we’ll get back faster than Swiggy delivery! 😄
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
